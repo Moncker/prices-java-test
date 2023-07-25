25581fd7500a16a4ffc818454d927b9be6e0001e
Moncker <ivanjmonferrer@gmail.com>
2023-07-25 18:39:21 +0200

    Initial commit

---
+++ src/main/java/com/moncker/pricesjavatest/PricesJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.moncker.pricesjavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesJavaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moncker/pricesjavatest/PricesJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moncker.pricesjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesJavaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
