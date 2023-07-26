f022b5602d2fc88f1d93f3b56608b94cbd3fb262
Moncker <ivanjmonferrer@gmail.com>
2023-07-26 11:28:26 +0200

    Initial database configurations

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO prices (price_id, brand_id, start_date, end_date, price_list, product_id, priority, price, curr ) VALUES
+  (1, 1, '2020-06-14 00:00:00' , '2020-12-31 23:59:59', 1, '35355', 0, 35.50, 'EUR');
+  
